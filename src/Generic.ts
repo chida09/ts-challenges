bf43559e3b30389222e4a9a57e8c622540c36bae
chida <t.chida0909@gmail.com>
2023-05-06 23:24:54 +0900

    add

--- src/Generic.ts
+++ src/Generic.ts
@@ -1,5 +1,5 @@
 /**
- * Generic使わない
+ * Object
  */
 type Data = {
   value1: number
@@ -12,9 +12,8 @@ const data: Data = {
 }
 console.log(data) // { value1: 123, value2: "hello" }
 
-/**
- * Generic使う
- */
+// -------------------------------------------
+
 type Data2<T, K> = {
   value1: T
   value2: K
@@ -25,3 +24,21 @@ const data2: Data2<number, string> = {
   value2: "hello"
 }
 console.log(data2) // { value1: 123, value2: "hello" }
+
+
+/**
+ * Function
+ */
+function test<T>(arg: T): T {
+  return arg;
+}
+
+console.log(test(1)); // 1
+console.log(test("Hello")); // "Hello"
+console.log(test(true)); // true
+
+// 引数2つ
+function test<T, K>(arg: T, arg2: K): T {
+  return arg
+}
+console.log(test(1, 2)); // 1
