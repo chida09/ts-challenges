d2de853aa9b4b059d1c8d01b272c82f220a8339a
chida <t.chida0909@gmail.com>
2023-05-07 23:14:59 +0900

    add

---
+++ src/intersection.ts
@@ -0,0 +1,19 @@
+/**
+ * 交差型のこと
+ * union型と対になるもの
+ * 2つの型T, Uに対してT & Uと書くと、TでもありUでもあるような型を表します。
+ */
+interface Hoge {
+  foo: string;
+  bar: number;
+}
+interface Piyo {
+  foo: string;
+  baz: boolean;
+}
+
+const obj: Hoge & Piyo = {
+  foo: 'foooooooo',
+  bar: 3,
+  baz: true,
+};
---
+++ src/partial-type.ts
@@ -0,0 +1,18 @@
+/**
+ * TypeScriptでは構造的部分型を採用しているため、次のようなことが可能です。
+ * MyObj2というのはfooプロパティだけを持つオブジェクトの型ですが、MyObj2型変数にMyObj型の値aを代入することができています。
+ * MyObj型の値はstring型のプロパティfooを持っているためMyObj2型の値の要件を満たしていると考えられるからです。
+ * ちなみに、一般にこのような場合MyObjはMyObj2の部分型であると言います。
+ */
+
+interface MyObj {
+  foo: string;
+  bar: number;
+}
+
+interface MyObj2 {
+  foo: string;
+}
+
+const a: MyObj = {foo: 'foo', bar: 3};
+const b: MyObj2 = a;
